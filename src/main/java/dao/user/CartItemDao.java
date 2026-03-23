f35ec77a001f8a86945eea9c70dac4a0a7092d38
ducphat0311@gmail.com <ducphat0311@gmail.com>
2026-03-23 23:29:54 +0700

    [FEAT]: viết dao lấy các thông tin của sản phẩm trong giỏ hàng

--- src/main/java/dao/user/CartItemDao.java
+++ src/main/java/dao/user/CartItemDao.java
@@ -1,6 +1,10 @@
 package dao.user;
 
 import dao.core.BaseDao;
+import model.CartItem;
+import model.Product;
+
+import java.util.List;
 
 public class CartItemDao extends BaseDao {
 
@@ -10,6 +14,48 @@ public class CartItemDao extends BaseDao {
                         .bind("cid", cartId)
                         .mapTo(int.class)
                         .one()
+        );
+    }
+
+    public List<CartItem> getItemsByCartId(int cartId) {
+        String sql = """
+        SELECT
+            ci.variant_id,
+            ci.quantity,
+            ci.price,
+            p.id   AS pid,
+            p.name,
+            p.thumbnail,
+            s.code AS size,
+            c.name AS color
+        FROM cart_items ci
+        JOIN product_variants v ON ci.variant_id = v.id
+        JOIN products p ON v.product_id = p.id
+        JOIN sizes s ON v.size_id = s.id
+        JOIN colors c ON v.color_id = c.id
+        WHERE ci.cart_id = :cid
+    """;
+
+        return getJdbi().withHandle(h ->
+                h.createQuery(sql)
+                        .bind("cid", cartId)
+                        .map((rs, ctx) -> {
+                            CartItem item = new CartItem();
+                            item.setVariantId(rs.getInt("variant_id"));
+                            item.setQuantity(rs.getInt("quantity"));
+                            item.setPrice(rs.getDouble("price"));
+                            item.setSize(rs.getString("size"));
+                            item.setColor(rs.getString("color"));
+
+                            Product p = new Product();
+                            p.setId(rs.getInt("pid"));
+                            p.setName(rs.getString("name"));
+                            p.setThumbnail(rs.getString("thumbnail"));
+
+                            item.setProduct(p);
+                            return item;
+                        })
+                        .list()
         );
     }
     
