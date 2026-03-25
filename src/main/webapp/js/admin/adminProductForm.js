b9a47c612f0de865734bea0cbb9ce18bc8788b04
Tran Thi Minh Thu <23130318@st.hcmuaf.edu.vn>
2026-03-26 00:58:35 +0700

    [UI]: Thêm giao diện form product

---
+++ src/main/webapp/js/admin/adminProductForm.js
@@ -0,0 +1,14 @@
+function previewProductImage(event) {
+    const file = event.target.files[0];
+    const preview = document.getElementById('product-image-preview');
+    
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+            preview.style.display = 'block';
+        }
+        reader.readAsDataURL(file);
+    }
+}
+
