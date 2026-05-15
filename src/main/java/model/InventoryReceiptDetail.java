8a5af719ed13510b71764b20a72cf92f8aa46748
Tran Thi Minh Thu <23130318@st.hcmuaf.edu.vn>
2026-05-16 01:33:55 +0700

    [FEAT]: Thêm model cho Quản lý kho

---
+++ src/main/java/model/InventoryReceiptDetail.java
@@ -0,0 +1,89 @@
+package model;
+
+import org.jdbi.v3.core.mapper.reflect.ColumnName;
+
+import java.io.Serializable;
+
+public class InventoryReceiptDetail implements Serializable {
+    private int id;
+    
+    @ColumnName("receipt_id")
+    private int receiptId;
+    
+    @ColumnName("product_variant_id")
+    private int productVariantId;
+    
+    private int quantity;
+    private double price;
+
+    private String productName;
+    private String colorName;
+    private String sizeName;
+
+    public InventoryReceiptDetail() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getReceiptId() {
+        return receiptId;
+    }
+
+    public void setReceiptId(int receiptId) {
+        this.receiptId = receiptId;
+    }
+
+    public int getProductVariantId() {
+        return productVariantId;
+    }
+
+    public void setProductVariantId(int productVariantId) {
+        this.productVariantId = productVariantId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getColorName() {
+        return colorName;
+    }
+
+    public void setColorName(String colorName) {
+        this.colorName = colorName;
+    }
+
+    public String getSizeName() {
+        return sizeName;
+    }
+
+    public void setSizeName(String sizeName) {
+        this.sizeName = sizeName;
+    }
+}
