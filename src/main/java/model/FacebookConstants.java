de6ec86fda5bdebe7465845b2cd7fc7013c0b20e
ducphat0311@gmail.com <ducphat0311@gmail.com>
2026-05-20 19:33:16 +0700

    [FIX]: thay đổi đg dẫn FB để chạy bth

--- src/main/java/model/FacebookConstants.java
+++ src/main/java/model/FacebookConstants.java
@@ -3,7 +3,7 @@ package model;
 public class FacebookConstants {
     public static final String FACEBOOK_APP_ID = System.getenv("FACEBOOK_APP_ID");
     public static final String FACEBOOK_APP_SECRET = System.getenv("FACEBOOK_APP_SECRET");
-    public static final String FACEBOOK_REDIRECT_URL = "http://localhost:8080/ShopQuanAo_war_exploded/auth/facebook";
+    public static final String FACEBOOK_REDIRECT_URL = "http://localhost:8080/store/auth/facebook";
 
     // url login FB
     public static final String FACEBOOK_LOGIN_URL = "https://www.facebook.com/v19.0/dialog/oauth?client_id="
