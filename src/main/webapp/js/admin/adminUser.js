d735af29442ec973ad41d7744101404cd70d50b2
Tran Thi Minh Thu <23130318@st.hcmuaf.edu.vn>
2026-06-07 01:35:43 +0700

    FIX]: Cập nhật UI và logic thêm tài khoản Quản lý người dùng

--- src/main/webapp/js/admin/adminUser.js
+++ src/main/webapp/js/admin/adminUser.js
@@ -1,14 +1,16 @@
 function openConfirmModal(userId, textStatus, textBtn) {
     document.getElementById("confirmUserId").value = userId;
-    document.getElementById("modalActionText").innerText = textBtn + " người dùng nầy?";
+    document.getElementById("modalActionText").innerText = textBtn + " người dùng";
     
     if (textBtn === 'khóa') {
          document.getElementById("formActionField").value = "block";
          document.getElementById("btnConfirmSubmit").className = "btn-danger";
     } else {
          document.getElementById("formActionField").value = "unblock";
-         document.getElementById("btnConfirmSubmit").className = "btn-primary"; 
+         document.getElementById("btnConfirmSubmit").className = "btn-success"; 
     }
+    
+    document.getElementById("btnConfirmSubmit").innerText = textBtn.charAt(0).toUpperCase() + textBtn.slice(1);
 
     document.getElementById("confirmModal").style.display = "flex";
 }
